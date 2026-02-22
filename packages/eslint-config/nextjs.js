dd52c3e0db94a9a83a007a2e175ae5a7975db8c3
tabrez-88 <tabrez.upwork88@gmail.com>
2026-02-22 20:52:05 +0800

    feat: Update ESLint configuration for TypeScript support and improved rules

--- packages/eslint-config/nextjs.js
+++ packages/eslint-config/nextjs.js
@@ -1,13 +1,56 @@
 /** @type {import("eslint").Linter.Config} */
 module.exports = {
-  extends: ['./base.js', 'next/core-web-vitals'],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 2022,
+    sourceType: 'module',
+  },
+  plugins: ['@typescript-eslint'],
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'next/core-web-vitals',
+    'prettier',
+  ],
   env: {
     browser: true,
     node: true,
+    es2022: true,
+  },
+  settings: {
+    'import/resolver': {
+      typescript: {
+        alwaysTryTypes: true,
+      },
+    },
   },
   rules: {
+    '@typescript-eslint/no-unused-vars': [
+      'error',
+      {
+        argsIgnorePattern: '^_',
+        varsIgnorePattern: '^_',
+      },
+    ],
+    '@typescript-eslint/explicit-function-return-type': 'off',
+    '@typescript-eslint/explicit-module-boundary-types': 'off',
+    '@typescript-eslint/no-explicit-any': 'warn',
+    'import/order': [
+      'error',
+      {
+        groups: ['builtin', 'external', 'internal', 'parent', 'sibling', 'index'],
+        'newlines-between': 'always',
+        alphabetize: {
+          order: 'asc',
+          caseInsensitive: true,
+        },
+      },
+    ],
+    'import/no-duplicates': 'error',
+    'no-console': ['warn', { allow: ['warn', 'error'] }],
     '@next/next/no-html-link-for-pages': 'off',
     'react/react-in-jsx-scope': 'off',
     'react/prop-types': 'off',
   },
+  ignorePatterns: ['node_modules', 'dist', 'build', '.turbo', 'coverage'],
 };
